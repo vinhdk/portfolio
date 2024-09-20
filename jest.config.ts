8a6001955b4bbc788d915685969f0fd0b1df0590
Administrator Aeiforge <administrator@aeiforge.com>
2024-09-20 22:57:40 +0700

    chore(main): init project

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'portfolio',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/portfolio',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
